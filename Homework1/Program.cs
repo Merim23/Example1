2268ba8c810724f6e7be536b6c8c6d102fe561bf
Meerim <meerimmambetova86@gmail.com>
2023-02-17 00:12:50 +0600

    Homework1

--- Homework1/Program.cs
+++ Homework1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// #1
+int num1 = 5;
+int num2 = 7;
+if(num1 > num2)
+{
+    Console.WriteLine("max = " + num1);
+}
+else
+{
+    Console.WriteLine("max = " + num2);
+}
+// #2
+// int max = 0;
+// int min = 0;
+
+// Console.Write("Введите первое число: ");
+// int firstNumber = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите второе число: ");
+// int secondNumber = Convert.ToInt32(Console.ReadLine());
+
+// if(firstNumber > secondNumber)
+// {
+//     max = firstNumber;
+//     min = secondNumber;
+// }
+// else
+// {
+//     max = secondNumber;
+//     min = firstNumber;
+// }
+// Console.WriteLine("max = " + max + " и " + "min = " + min);
